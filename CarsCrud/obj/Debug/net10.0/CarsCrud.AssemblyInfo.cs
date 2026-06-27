5bdaef3a7ed964da8c71d295ab0659dbdf80ad4b
NezoxM3 <nezox150fps@gmail.com>
2026-06-27 12:07:05 -0500

    Added the remainings

--- CarsCrud/obj/Debug/net10.0/CarsCrud.AssemblyInfo.cs
+++ CarsCrud/obj/Debug/net10.0/CarsCrud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarsCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f365c26a2a065f16c6e0b512888c6dc16eb2282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c27ac41235e62f4a926e5c2b14d3d8ce8261b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarsCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarsCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProductImportApp/obj/Debug/net10.0/ProductImportApp.AssemblyInfo.cs
+++ ProductImportApp/obj/Debug/net10.0/ProductImportApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductImportApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d99308e00c527f6a328c8d08effe06cd6857866")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c27ac41235e62f4a926e5c2b14d3d8ce8261b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductImportApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductImportApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
